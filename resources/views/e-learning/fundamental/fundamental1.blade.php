3e70d4c24c26ac16c55fab92e8b2f9cc58ce757f
RES27 <eggy.riski27@gmail.com>
2024-06-18 19:46:26 +0700

    mengubah elerning

--- resources/views/e-learning/fundamental/fundamental1.blade.php
+++ resources/views/e-learning/fundamental/fundamental1.blade.php
@@ -23,26 +23,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-3 d-none d-md-block"> <!-- Kolom ini akan ditampilkan di samping iframe pada tampilan tablet dan desktop -->
-                    <div class="card h-100">
-                        <div class="card-body d-flex flex-column">
-                            <!-- tempat komentar -->
-                            <p></p>
-                        </div>
-                        <div class="card-footer">
-                            <form action="">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <input type="text" class="form-control" name="komentar" id="komentar" placeholder="Komentar">
-                                    </div>
-                                    <div class="col-12 mt-2">
-                                        <button type="submit" class="btn btn-success btn-block">Kirim</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
